dadd95ae62ee1df412cb018bf7e3cbf2f433b439
faylan2 <faylan2@sina.com>
2018-04-13 10:36:38 +0800

    上传C#代码

---
+++ A2.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class A1 : MonoBehaviour
+{
+
+    private Transform s;
+    void Start()
+    {
+        s = gameObject.GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.I))
+        {
+            s.Translate(Vector3.forward * 0.3f, Space.World);
+
+        }
+        if (Input.GetKey(KeyCode.K))
+        {
+            s.Translate(Vector3.back * 0.3f, Space.World);
+
+        }
+        if (Input.GetKey(KeyCode.J))
+        {
+            s.Translate(Vector3.left * 0.3f, Space.World);
+
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            s.Translate(Vector3.right * 0.3f, Space.World);
+
+        }
+
+
+    }
+
+}
